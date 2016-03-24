ed51a9c731bc6f0c64f16a5ba02f0a16ed8d84a5
yuzhou <yuzhouisme@me.com>
2016-03-24 16:57:53 +0800

    init project

---
+++ src/component/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+
+const App = React.createClass({
+  render() {
+    return (
+      <DatePicker />
+    );
+  },
+});
+
+export default App;
