dcb7eb5766b2d0f2e9a35e9c6d43c8684f6304e4
yuzhou <yuzhouisme@me.com>
2016-03-24 20:35:00 +0800

    modify index and screenshot.png

--- src/component/front/Main.js
+++ src/component/front/Main.js
@@ -3,10 +3,45 @@ import React from 'react';
 var Main = React.createClass({
   render: function() {
     return (
-      <div>
-        <p>Hello react && antd, :)</p>
+      <div style={{
+          width: window.innerWidth,
+          height: window.innerHeight,
+          marginTop: 0,
+          background: `url(./static/img/example/Slider_Pequeno.jpg)`,
+          backgroundSize: 'cover', backgroundRepeat: 'no-repeat',
+        }}>
+        <div style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 50,
+            color: '#FFFFFF',
+            fontSize: '1.8em',
+            fontWeight: 400
+          }}>
+          <div style={{
+              letterSpacing: 1
+            }}>
+            <p style={{
+                marginTop: 26,
+                marginBottom: 20,
+                fontSize: '2em',
+                fontWeight: 900
+              }}>Hello react && antd, :)</p>
+            <p style={{
+                marginBottom: 20,
+                fontSize: '2.2em'
+              }}></p>
+            <p style={{
+                marginBottom: 20
+              }}></p>
+            <p style={{
+                marginBottom: 6
+              }}></p>
+          </div>
+        </div>
+
       </div>
-    )
+    );
   }
 });
 
